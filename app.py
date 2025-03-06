c5a48d3d297cd5e2c8593787e488db6b387291ba
Francesco Pettenon <francescopettenon99@gmail.com>
2025-03-06 12:43:55 +0000

    temp: see users

--- app.py
+++ app.py
@@ -37,6 +37,11 @@ CREATE TABLE IF NOT EXISTS flashcards (
 """)
 conn.commit()
 
+if st.sidebar.button("📋 Mostra utenti"):
+    st.sidebar.subheader("Lista utenti registrati")
+    df_users = pd.read_sql_query("SELECT id, username FROM users", conn)
+    print(df_users)
+
 # ---- Funzione per registrare un utente ----
 def register_user(username, password):
     hashed_pw = hashlib.sha256(password.encode()).hexdigest()
